6cae785d8f32e7a9979f79f75a4e83c4440d4135
Leonardo Alonso <leonardoalonsososa@gmail.com>
2019-11-10 21:12:45 -0600

    WIP: Get clients test

--- resources/Clientes/model.py
+++ resources/Clientes/model.py
@@ -2,10 +2,32 @@ from app import db, ma
 from flask import Flask
 from marshmallow import Schema, fields, pre_load, validate
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.mysql import TINYINT
+import uuid
 
 
 class Cliente(db.Model):
-    pass
+    __tablename__ = 'Clientes'
+    id = db.Column(db.String(36), primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    is_facebook = db.Column(TINYINT(1), default=0)
+    is_twitter = db.Column(TINYINT(1), default=0)
+    is_google = db.Column(TINYINT(1), default=0)
+    profile_picture = db.Column(db.String(255), nullable=True)
+    creation_date = db.Column(
+        db.TIMESTAMP, server_default=db.func.current_timestamp(), nullable=False)
+
+    def __init__(self, **kwargs):
+        self.id = uuid.uuid1()
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
 
 class ClienteSchema(ma.Schema):
-    pass
+    id = fields.Integer()
+    name = fields.String(required=True)
+    email = fields.String(required=True)
+    profile_picture = fields.String(required=True)
+
--- resources/Clientes/views.py
+++ resources/Clientes/views.py
@@ -1,9 +1,14 @@
 from flask_restful import Resource
+from .model import Cliente, ClienteSchema
 
+clients_schema = ClienteSchema(many=True)
+client_schema = ClienteSchema()
 
 class Cliente(Resource):
     def get(self):
-        return {"message": "Hello, World!"}
+        clients = Cliente.query.all()
+        clients = clients_schema.dump(clients)
+        return {'status': 'success', 'data': clients}, 200
     
     def post(self):
         return {"message": "Hello, World!"}
