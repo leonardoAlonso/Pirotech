95bc328d33fb12dc5ec719c8e4d8d04c2f2e1906
Leonardo Alonso <leonardoalonsososa@gmail.com>
2020-03-17 19:59:09 -0600

    Remove manager

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate
 from api import create_app
 from api.config import DevelopmentConfig
 from api.models import db
@@ -8,10 +7,7 @@ from api.Users.models import User
 
 app = create_app(DevelopmentConfig)
 migrate = Migrate(app, db)
-manager = Manager(app)
-
-manager.add_command('db', MigrateCommand)
 
 
 if __name__ == "__main__":
-    manager.run()
+    app.run()
